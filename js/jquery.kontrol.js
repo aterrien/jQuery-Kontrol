cbe3f2534574410d8961803d428e68a8bb31aad6
aterrien <anthony.terrien@gmail.com>
2012-07-02 23:55:37 +0200

    fixed a bug in angle computation

--- js/jquery.kontrol.js
+++ js/jquery.kontrol.js
@@ -488,14 +488,23 @@ $(function () {
         };
 
         this.xy2val = function (x, y) {
-            var b, a, ret;
-            b = a = Math.atan2(
+            var a, ret;
+            
+            a = Math.atan2(
                         x - (this.x + this.w2)
                         , - (y - this.y - this.w2)
                     ) - this.angleOffset;
-            (a < 0) && (b = a + this.angleArc);
-            ret = Math.round(b * (this.options.max - this.options.min) / this.angleArc)
+
+            if(this.angleArc != this.PI2 && (a < 0) && (a > -.5)) {
+                // if isset angleArc option, set to min if .5 under min
+                a = 0;
+            } else if (a < 0) {
+                a += this.PI2;
+            }
+
+            ret = Math.round(a * (this.options.max - this.options.min) / this.angleArc)
                     + this.options.min;
+
             this.options.stopper
                 && (ret = Math.max(Math.min(ret, this.options.max), this.options.min));
 
