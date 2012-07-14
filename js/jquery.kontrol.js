7f779ecb722bd9c70bd592feb5ddaae8b803d9c9
aterrien <anthony.terrien@gmail.com>
2012-07-14 15:17:29 +0200

    fix touchMove bug

--- js/jquery.kontrol.js
+++ js/jquery.kontrol.js
@@ -217,12 +217,14 @@ $(function () {
 
             var touchMove = function (e) {
                 var v = self._touchCapture(e).xy2val(self.dx, self.dy);
-                if (v == this.newValue) return;
+
+                if (v == self.newValue) return;
 
                 if (
                     self.changeHook
                     && (self.changeHook(v) === false)
                 ) return;
+
 
                 self.change(v);
                 self._draw();
